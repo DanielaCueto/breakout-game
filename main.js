6004414ac70c021e9edf622534bfb504c14b570d
Daniela <danisalazarcueto7@gmail.com>
2022-01-19 16:22:14 +0000

    add interactivity to the game

--- main.js
+++ main.js
@@ -2,52 +2,138 @@
 const canvas = document.querySelector("canvas");
 const context = canvas.getContext("2d");
 
-const canvasW = canvas.width; 
-const canvasH = canvas.height
+const canvasW = canvas.width;
+const canvasH = canvas.height;
 
 // velocidad a la que la pelota se mueve. Ritmo de cambio de x e y
-let vx = 40;
-let vy = 40;
+let ballVX = 40;
+let ballVY = 40;
 
-// dibuja la posición de la pelota 
-let x = 300;
-let y = 200;
+// dibuja la posición de la pelota
+let ballX = 300;
+let ballY = 200;
+let ballRadius = 10;
 const refreshRate = 1 / 60;
 
-function updateBall() {
-  x = x + vx * refreshRate;
-  y = y + vy * refreshRate;
-  if (y >= canvasH){
-    //verifico si hay colisión con la pared inferior. 
-    y = canvasH;
-    vy *= -1;
-  } else if(y <= 0){
-    //verifico si hay colisión con la pared superior.
-    y = 0;
-    vy *= -1;
-  }
-  if (x >= canvasW) {
-    //verifico si hay colisión con la pared del lado derecho 
-    x = canvasW; 
-    vx *= -1;
-  } else if (x<= 0){
-    //verifico si hay colisión con la pared del lado izquierdo.
-    x = 0; 
-    vx *= -1;
-  }
+// movimiento de la barra
+const barW = 150;
+const barH = 20;
+let barVX = 0;
+let barVY = 0;
+let barX = canvasW / 2 - barW / 2;
+let barY = canvasH - barH;
 
+// teclas
+let playerPressingLeft = false; 
+let playerPressingRight = false; 
+
+function updateBall() {
+  ballX = ballX + ballVX * refreshRate;
+  ballY = ballY + ballVY * refreshRate;
+  if (ballY >= canvasH) {
+    //verifico si hay colisión con la pared inferior.
+    ballY = canvasH;
+    ballVY *= -1;
+  } else if (ballY <= 0) {
+    //verifico si hay colisión con la pared superior.
+    ballY = 0;
+    ballVY *= -1;
+  }
+  if (ballX >= canvasW) {
+    //verifico si hay colisión con la pared del lado derecho
+    ballX = canvasW;
+    ballVX *= -1;
+  } else if (ballX <= 0) {
+    //verifico si hay colisión con la pared del lado izquierdo
+    ballX = 0;
+    ballVX *= -1;
+  }
+  const barRectangle =  {
+    
+    x: barX,
+    y: barY,
+    w: barW,
+    h: barH,
+  }; 
+  const ballRectangle = 
+  {
+    x: ballX - ballRadius,
+    y: ballY - ballRadius,
+    w: 2 * ballRadius,
+    h: 2 * ballRadius,
+  }; 
+
+  const ballVelocity = 
+  {
+    vx: ballVX,
+    vy: ballVY,
+  }
+  let barCollision = detectRectangleCollision(
+    barRectangle, ballRectangle, ballVelocity
+  );
+  if (barCollision !== null){
+      ballX = barCollision.newPos.x + ballRadius; 
+      ballY = barCollision.newPos.y + ballRadius;
+      if (barCollision.reason.x === true){
+        ballVX *= -1;
+      } 
+      if (barCollision.reason.y === true){
+        ballVY *= -1; 
+      }
+      
+  }
 }
-function drawBackGround (){
-    drawRectangle(context, 0, 0, canvasW, canvasH, "#E0BBE4");
+function updateBar() {
+  if (playerPressingRight){
+    barVX = 60; 
+  }else if (playerPressingLeft){
+    barVX = -60; 
+  } else{
+    barVX = 0 ; 
+  }
+  barX = barX + barVX * refreshRate;
+  if (barX >= canvasW - barW) {
+    barX = canvasW - barW;
+    barVX *= -1;
+  } else if (barX <= 0) {
+    barX = 0;
+    barVX *= -1;
+  }
+}
+function drawBackGround() {
+  drawRectangle(context, 0, 0, canvasW, canvasH, "#E0BBE4");
 }
 function drawBall() {
-  drawCircle(context, x, y, 10, "#fff");
+  drawCircle(context, ballX, ballY, ballRadius, "#fff");
+}
+function drawBar() {
+  drawRectangle(context, barX, barY, barW, barH, "#000");
 }
 
 function updateAndDrawScene() {
-
   updateBall();
+  updateBar();
   drawBackGround();
   drawBall();
+  drawBar();
 }
 setInterval(updateAndDrawScene, refreshRate);
+
+window.addEventListener('keydown', (event)=>{
+  let key = event.key; 
+ 
+  if(key === 'ArrowRight'){
+    playerPressingRight = true; 
+  } else if (key === 'ArrowLeft'){
+    playerPressingLeft = true; 
+  }
+})
+window.addEventListener('keyup', (event)=>{
+  let key = event.key; 
+  
+  if(key === 'ArrowRight'){
+    playerPressingRight= false; 
+  } else if (key === 'ArrowLeft'){
+    playerPressingLeft = false; 
+  }
+})
--- utils.js
+++ utils.js
@@ -15,7 +15,7 @@ function drawCircle(context, x, y, r, color) {
 /**
  * Generic collision detection system between 2 rectangles, assuming the
  * second one is moving according to a certain speed vector.
- * 
+ *
  * @param {object} rect1 {x,y,w,h} description of first rectangle (assumed static).
  * @param {object} rect2 {x,y,w,h} description of second rectangle (assumed moving).
  * @param {object} velocity2 {vx,vy} description of movement of second rectangle.
@@ -23,9 +23,9 @@ function drawCircle(context, x, y, r, color) {
  *   * rewindTime: float value representing the amount of seconds that have to be
  *                 rewinded to remove the collision according to the velocity of
  *                 rect2.
- *   * reason: object containing 2 boolean properties, x and y. x (y) will be 
+ *   * reason: object containing 2 boolean properties, x and y. x (y) will be
  *             true if the collision happened mainly because of vx (vy) and false
- *             otherwise. It may happen that both x and y are true (e.g. diagonal 
+ *             otherwise. It may happen that both x and y are true (e.g. diagonal
  *             movement with same velocity components).
  *   * newPos: object containing updated rect2 x and y values corresponding to the
  *             rewinding of x2 and y2 based on vx2 and vy2 and rewindTime.
@@ -33,9 +33,11 @@ function drawCircle(context, x, y, r, color) {
 function detectRectangleCollision(rect1, rect2, velocity2) {
   let horizontalCollision = !(
     // rect 1 in lower x and there's a gap with respect to rect 2
-    rect1.x + rect1.w < rect2.x ||
-    // rect 2 in lower x and there's a gap with respect to rect 1
-    rect2.x + rect2.w < rect1.x
+    (
+      rect1.x + rect1.w < rect2.x ||
+      // rect 2 in lower x and there's a gap with respect to rect 1
+      rect2.x + rect2.w < rect1.x
+    )
   );
   if (!horizontalCollision) {
     // If no horizontal collision, there can't be a collision at all
@@ -43,49 +45,52 @@ function detectRectangleCollision(rect1, rect2, velocity2) {
   }
   let verticalCollision = !(
     // rect 1 in lower y and there's a gap with respect to rect 2
-    rect1.y + rect1.h < rect2.y ||
-    // rect 2 in lower y and there's a gap with respect to rect 1
-    rect2.y + rect2.h < rect1.y
-  )
+    (
+      rect1.y + rect1.h < rect2.y ||
+      // rect 2 in lower y and there's a gap with respect to rect 1
+      rect2.y + rect2.h < rect1.y
+    )
+  );
   if (!verticalCollision) {
     // If no vertical collision, there can't be a collision at all
     return null;
   }
 
-  // If we got this far, that means there's both an horizontal and a vertical collision. 
-  // We now have to figure out how much to backtrack the movement of rect 2 based on its 
+  // If we got this far, that means there's both an horizontal and a vertical collision.
+  // We now have to figure out how much to backtrack the movement of rect 2 based on its
   // velocity so that the collision goes away.
   // This is done by figuring out the quickest way to remove either horizontal or
   // vertical collision and "rewinding" time according to that amount.
-  let newX2;
+  let collisionFreeX2;
   if (velocity2.vx > 0) {
     // if moving from left to right, new x2 position is such that x2 + w2 = x1
-    newX2 = rect1.x - rect2.w;
+    collisionFreeX2 = rect1.x - rect2.w;
   } else {
     // else we're moving from right to left, new x2 position is such that x2 = x1 + w1
-    newX2 = rect1.x + rect1.w;
+    collisionFreeX2 = rect1.x + rect1.w;
   }
-  let rewindTimeX = (rect2.x - newX2) / velocity2.vx;
+  let rewindTimeX = (rect2.x - collisionFreeX2) / velocity2.vx;
 
-  let newY2;
+  let collisionFreeY2;
   if (velocity2.vy > 0) {
     // if moving from top to bottom, new y2 position is such that y2 + h2 = y1
-    newY2 = rect1.y - rect2.h;
+    collisionFreeY2 = rect1.y - rect2.h;
   } else {
     // else we're moving from bottom to top, new y2 position is such that y2 = y1 + h1
-    newY2 = rect1.y + rect1.h;
+    collisionFreeY2 = rect1.y + rect1.h;
   }
-  let rewindTimeY = (rect2.y - newY2) / velocity2.vy;
+  let rewindTimeY = (rect2.y - collisionFreeY2) / velocity2.vy;
+  let minRewindTime = Math.min(rewindTimeX, rewindTimeY);
 
   return {
-    rewindTime: Math.min(rewindTimeX, rewindTimeY),
+    rewindTime: minRewindTime,
     reason: {
       x: rewindTimeX <= rewindTimeY,
       y: rewindTimeY <= rewindTimeX,
     },
     newPos: {
-      x: newX2,
-      y: newY2,
-    }
+      x: rect2.x - velocity2.vx * minRewindTime,
+      y: rect2.y - velocity2.vy * minRewindTime,
+    },
   };
 }
